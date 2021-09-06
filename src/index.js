781864d3b33b2fd6dde84b5aaa6f8e3b8e78276c
Ryan Kubik <rmkubik@me.com>
2021-09-06 10:35:06 -0700

    Update lockfile and change text

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import React from "react";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return <h1>Sproink!</h1>;
 };
 
 const root = document.getElementById("root");
