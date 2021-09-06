a3886cb4564dc4bd2fb1b7c3a03cdc82b71e5b04
Ryan Kubik <rmkubik@me.com>
2021-09-06 11:08:13 -0700

    Create the button and bg and audio

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import ReactDOM from "react-dom";
-import React from "react";
+import React, { useRef } from "react";
+import "./styles.scss";
+import sproinkClip from "../assets/sproink.mp3";
 
 const App = () => {
-  return <h1>Sproink!</h1>;
+  const sproinkAudioRef = useRef();
+
+  return (
+    <>
+      <audio ref={sproinkAudioRef} src={sproinkClip}></audio>
+      <button
+        onClick={() => {
+          sproinkAudioRef.current.currentTime = 0;
+          sproinkAudioRef.current.play();
+        }}
+      >
+        Sproink!
+      </button>
+    </>
+  );
 };
 
 const root = document.getElementById("root");
